6c392d600121aab7dca62244b250260ef82ef512
Diego D. Gómez <gomezdiego1902@gmail.com>
2025-03-17 12:18:21 -0300

    Cambios en el frontend-Login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -46,66 +46,66 @@ const Login = () => {
         <div className='h-full w-full lg:w-2/3 flex flex-col items-center justify-center'>
           <div className='w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-10 2xl:-mt-20'>
             <span className='flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base bordergray-300 text-gray-600'>
-              Manage all your task in one place!
+            
             </span>
-            <p className='flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-blue-700'>
-              <span>Cloud-Based</span>
-              <span>Task Manager</span>
+            <p className='flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-red-700'>
+            
+              <span>Administrador de Tareas de la Unidad de Proyectos, Convenios e Investigación</span>
             </p>
 
             <div className='cell'>
-              <div className='circle rotate-in-up-left'></div>
+              <div className='circle rotate-in-up-lef'></div>
             </div>
           </div>
         </div>
 
-        {/* right side */}
+        {/* derecha */}
         <div className='w-full md:w-1/3 p-4 md:p-1 flex flex-col justify-center items-center'>
           <form
             onSubmit={handleSubmit(submitHandler)}
             className='form-container w-full md:w-[400px] flex flex-col gap-y-8 bg-white px-10 pt-14 pb-14'
           >
             <div className=''>
-              <p className='text-blue-600 text-3xl font-bold text-center'>
-                Welcome back!
+              <p className='text-red-600 text-3xl font-bold text-center'>
+                Bienvenido de nuevo!
               </p>
               <p className='text-center text-base text-gray-700 '>
-                Keep all your credential safge.
+               
               </p>
             </div>
 
             <div className='flex flex-col gap-y-5'>
               <Textbox
-                placeholder='email@example.com'
+                placeholder=''
                 type='email'
                 name='email'
-                label='Email Address'
+                label='Email'
                 className='w-full rounded-full'
                 register={register("email", {
-                  required: "Email Address is required!",
+                  required: "*Campo obligatorio!",
                 })}
                 error={errors.email ? errors.email.message : ""}
               />
               <Textbox
-                placeholder='your password'
+                placeholder=''
                 type='password'
                 name='password'
-                label='Password'
+                label='Contraseña'
                 className='w-full rounded-full'
                 register={register("password", {
-                  required: "Password is required!",
+                  required: "*Campo obligatorio!",
                 })}
                 error={errors.password ? errors.password.message : ""}
               />
 
               <span className='text-sm text-gray-500 hover:text-blue-600 hover:underline cursor-pointer'>
-                Forget Password?
+                Ha olvidado su contraseña?
               </span>
 
               <Button
                 type='submit'
-                label='Submit'
-                className='w-full h-10 bg-blue-700 text-white rounded-full'
+                label='Ingresar'
+                className='w-full h-10 bg-red-700 text-white rounded-full'
               />
             </div>
           </form>
