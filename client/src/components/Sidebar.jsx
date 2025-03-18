750e4ae760886f719357accc45f5004aec00ccb1
Diego D. Gómez <gomezdiego1902@gmail.com>
2025-03-18 08:47:05 -0300

    ajustes del Sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {
   MdDashboard,
   MdOutlineAddTask,
+  MdOutlineAdjust,
   MdOutlinePendingActions,
   MdSettings,
   MdTaskAlt,
@@ -14,27 +15,27 @@ import clsx from "clsx";
 
 const linkData = [
   {
-    label: "Dashboard",
+    label: "Inicio",
     link: "dashboard",
     icon: <MdDashboard />,
   },
   {
-    label: "Tasks",
+    label: "Tareas",
     link: "tasks",
     icon: <FaTasks />,
   },
   {
-    label: "Completed",
+    label: "Completadas",
     link: "completed/completed",
     icon: <MdTaskAlt />,
   },
   {
-    label: "In Progress",
+    label: "En progreso",
     link: "in-progress/in progress",
-    icon: <MdOutlinePendingActions />,
+    icon: <MdOutlineAdjust />,
   },
   {
-    label: "To Do",
+    label: "Pendientes",
     link: "todo/todo",
     icon: <MdOutlinePendingActions />,
   },
@@ -44,7 +45,7 @@ const linkData = [
     icon: <FaUsers />,
   },
   {
-    label: "Trash",
+    label: "Papelera",
     link: "trashed",
     icon: <FaTrashAlt />,
   },
@@ -70,22 +71,22 @@ const Sidebar = () => {
         to={el.link}
         onClick={closeSidebar}
         className={clsx(
-          "w-full lg:w-3/4 flex gap-2 px-3 py-2 rounded-full items-center text-gray-800 text-base hover:bg-[#2564ed2d]",
-          path === el.link.split("/")[0] ? "bg-blue-700 text-neutral-100" : ""
+          "w-full lg:w-3/4 flex gap-2 px-3 py-2 rounded-full items-center text-gray-800 text-base hover:bg-[#1a9f10] hover:text-white",
+          path === el.link.split("/")[0] ? "bg-red-700 text-neutral-100" : ""
         )}
       >
         {el.icon}
-        <span className='hover:text-[#2564ed]'>{el.label}</span>
+        <span className='hover:text-[#010a00]'>{el.label}</span>
       </Link>
     );
   };
   return (
     <div className='w-full  h-full flex flex-col gap-6 p-5'>
       <h1 className='flex gap-1 items-center'>
-        <p className='bg-blue-600 p-2 rounded-full'>
+        <p className='bg-red-600 p-2 rounded-full'>
           <MdOutlineAddTask className='text-white text-2xl font-black' />
         </p>
-        <span className='text-2xl font-bold text-black'>TaskMe</span>
+        <span className='text-2xl font-bold text-black'>UPCI</span>
       </h1>
 
       <div className='flex-1 flex flex-col gap-y-5 py-8'>
@@ -97,7 +98,7 @@ const Sidebar = () => {
       <div className=''>
         <button className='w-full flex gap-2 p-2 items-center text-lg text-gray-800'>
           <MdSettings />
-          <span>Settings</span>
+          <span>Configuraciones</span>
         </button>
       </div>
     </div>
