254839e7688e7a945f654b253fe3472003d93e99
Diego D. Gómez <gomezdiego1902@gmail.com>
2025-11-03 09:51:14 -0300

    Agregar plantilla firebase.example.js y corregir configuración

--- client/src/firebase.example.js
+++ client/src/firebase.example.js
@@ -5,10 +5,10 @@ import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
   authDomain: "tu-proyecto.firebaseapp.com",
-  projectId: "tu-proyecto",
-  storageBucket: "tu-proyecto.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:xxxxxxxxxxxxxx",
+  projectId: "todoapp-651a5",
+  storageBucket: "todoapp-651a5.appspot.com",
+  messagingSenderId: "55781163126",
+  appId: "1:55781163126:web:7aab129604d34275e35f57",
 };
 
 export const app = initializeApp(firebaseConfig);
