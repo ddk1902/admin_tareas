59443d97c02ff58743650c3a0c93821ec89d80ee
Diego D. Gómez <gomezdiego1902@gmail.com>
2025-11-03 10:41:19 -0300

    Build exitoso local, listo para deploy en Vercel

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -1,9 +1,7 @@
 import { initializeApp } from "firebase/app";
-<<<<<<< HEAD
-import { getStorage } from "firebase/storage"
-=======
+
 import { getStorage } from "firebase/storage";
->>>>>>> 70d585e (Build exitoso local, listo para deploy en Vercel)
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
   authDomain: "todoapp-651a5.firebaseapp.com",
@@ -14,8 +12,5 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
-<<<<<<< HEAD
 export const storage = getStorage(app);
-=======
-export const storage = getStorage(app);
->>>>>>> 70d585e (Build exitoso local, listo para deploy en Vercel)
+s
