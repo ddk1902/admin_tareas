53b775ef9e976de8a29ead00a1af7668b822d1d5
Diego D. Gómez <gomezdiego1902@gmail.com>
2025-11-04 10:19:38 -0300

    Ajuste de CORS

--- server/index.js
+++ server/index.js
@@ -22,6 +22,7 @@ app.use(
     origin: [
       "http://localhost:3000",
       "https://admin-tareas.onrender.com",  // backend
+      "https://admin-tareas.vercel.app", // frontend
       "http://localhost:5173",              // para desarrollo local
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
