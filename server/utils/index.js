6bb45d62571ef372727b3eec18399fbc187d150f
Diego D. Gómez <gomezdiego1902@gmail.com>
2025-11-05 11:45:19 -0300

    Ajuste de JWT

--- server/utils/index.js
+++ server/utils/index.js
@@ -22,7 +22,7 @@ export const createJWT = (res, userId) => {
   // Change sameSite from strict to none when you deploy your app
   res.cookie("token", token, {
   httpOnly: true,
-  secure: process.env.NODE_ENV !== "development",
+  secure: process.env.NODE_ENV === "development",
   sameSite: "strict",
   maxAge: 1 * 24 * 60 * 60 * 1000,
 });
