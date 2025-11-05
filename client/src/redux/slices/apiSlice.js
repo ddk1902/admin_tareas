c945befda543ba2c81c9269a6a89c1efd22f420e
Diego D. Gómez <gomezdiego1902@gmail.com>
2025-11-05 11:28:22 -0300

    Ajuste de rutas

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -2,6 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // Base URL para tu API (Render)
 const API_URI = import.meta.env.VITE_APP_BASE_URL;
+console.log("🌍 Base API:", import.meta.env.VITE_APP_BASE_URL);
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
